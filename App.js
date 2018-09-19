03d6ff2b88d7c7590a0bde7f4904eb616c35ec63
willedanielsson <william.l.danielsson@gmail.com>
2018-09-19 11:11:15 +0200

    Added Swiper

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Swiper from 'react-native-deck-swiper'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Swiper
+            cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+            renderCard={(card) => {
+                return (
+                    <View style={styles.card}>
+                        <Text style={styles.text}>{card}</Text>
+                    </View>
+                )
+            }}
+            onSwiped={(cardIndex) => {console.log(cardIndex)}}
+            onSwipedAll={() => {console.log('onSwipedAll')}}
+            cardIndex={0}
+            backgroundColor={'#4FD0E9'}
+            stackSize= {3}>
+            <Button
+                onPress={() => {console.log('oulala')}}
+                title="Press me">
+                You can press me
+            </Button>
+        </Swiper>
+    </View>
     );
   }
 }
@@ -16,8 +34,19 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5FCFF"
   },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
+  }
 });
