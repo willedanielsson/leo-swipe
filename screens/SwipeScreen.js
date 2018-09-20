3a47f55b790f73484837c6b6271bce101f393437
willedanielsson <william.l.danielsson@gmail.com>
2018-09-20 16:56:22 +0200

    Improved swiper

--- screens/SwipeScreen.js
+++ screens/SwipeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Swiper from 'react-native-deck-swiper'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import Card from '../components/Cards'
 import DATA from '../mockData/bets'
 
@@ -39,6 +39,9 @@ export default class SwipeScreen extends React.Component {
             backgroundColor={'#f7f7f7'}
             stackSize={3}
             cardVerticalMargin={100}
+            marginBottom={100}
+            infinite={true}
+            overlayLabels={labelConfig}
             >
         </Swiper>
       </View>
@@ -48,6 +51,57 @@ export default class SwipeScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#f7f7f7'
   }
 });
+
+
+const labelConfig = {
+  left: {
+    title: 'NOPE',
+    style: {
+      label: {
+        backgroundColor: '#e60000',
+        color: 'white'
+      },
+      wrapper: {
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        justifyContent: 'flex-start',
+        marginTop: 30,
+        marginLeft: -30
+      }
+    }
+  },
+  right: {
+    title: 'BET',
+    style: {
+      label: {
+        backgroundColor: '#1daa63',
+        color: 'white'
+      },
+      wrapper: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        marginTop: 30,
+        marginLeft: 30
+      }
+    }
+  },
+  top: {
+    title: 'COMBO',
+    style: {
+      label: {
+        backgroundColor: '#fcbe31',
+        color: 'white'
+      },
+      wrapper: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
+    }
+  }
+}
